a93a6bbb0e5f5d4bc00d14cf51169c7e86935df3
xXDNDXx <xXDNDXx@users.noreply.github.com>
2026-09-03 12:52:29 +0000

    deploy: cb4dc6f52a5567ab27903d9ab637281a7dd36ae5

--- src/components/auth/GoogleOAuthModal.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-// Neutered: All login and 1-click save features are now handled cleanly by UserMenu.tsx
-export const GoogleOAuthModal: React.FC<any> = () => null;
--- src/components/auth/GoogleSignInButton.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { UserMenu } from './UserMenu';
-
-export const GoogleSignInButton: React.FC<any> = () => {
-  return <UserMenu />;
-};
