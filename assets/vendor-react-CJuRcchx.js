38d340ad9b66d6ef4e92664f6815c6caf56105b4
xXDNDXx <xXDNDXx@users.noreply.github.com>
2026-09-04 13:22:10 +0000

    deploy: 4a59ee8c23f17e82ee8ddfc1c57241d767cc2e44

--- assets/vendor-react-CJuRcchx.js
+++ assets/vendor-react-CJuRcchx.js
@@ -1,4 +1,4 @@
-import{r as yc}from"./vendor-ui-k8TjXgJS.js";var hi={exports:{}},ve={},yi={exports:{}},gi={};/**
+import{r as yc}from"./vendor-ui-DEQdev4l.js";var hi={exports:{}},ve={},yi={exports:{}},gi={};/**
  * @license React
  * scheduler.production.min.js
  *
